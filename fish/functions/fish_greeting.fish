67d3d95feae1135c9372612b2b3f047730734e1e
ayunami2000 <spwilliamsiam@gmail.com>
2022-11-28 21:07:39 -0500

    Fix typo.

--- fish/functions/fish_greeting.fish
+++ fish/functions/fish_greeting.fish
@@ -1,5 +1,5 @@
 function fish_greeting
-echo (set_color AAA -i)\""I, "(set_color 571ea8 -o)"Lyicx. "(set_color normal; set_color AAA -i)"do swear that i will be faithful and bear true allegience to "(set_color 0F0 -o)"Ren"
+echo (set_color AAA -i)\""I, "(set_color 571ea8 -o)"Lyicx. "(set_color normal; set_color AAA -i)"do swear that I will be faithful and bear true allegience to "(set_color 0F0 -o)"Ren"
 echo (set_color normal; set_color AAA -i)"according to law. "(set_color F00 -o)"So help me god."(set_color normal; set_color AAA -i)"\""
 echo (set_color normal)
 echo (set_color -o brmagenta)"- 31/05/22 <3"
